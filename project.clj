e9036d1cc7e9e07d08747bd9748c570bb97932af
Juan Noria <jnoria@quantumlabs.ai>
2024-04-24 14:55:40 -0400

    Create project.clj

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject caudal "0.8.0"
+    :description "Caudal Platform"
+    :url "http://caudal.io/"
+    :license {:name "Eclipse Public License"
+              :url "http://www.eclipse.org/legal/epl-v10.html”})
