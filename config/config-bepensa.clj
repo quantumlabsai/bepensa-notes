8bf4015d34c449494862a175559db4a30d7fa864
Felipe de Jesus Gerard Contreras <felipe.gerard.c@gmail.com>
2026-07-09 12:43:14 -0600

    Update antenna configuration for improved reception

--- config/config-bepensa.clj
+++ config/config-bepensa.clj
@@ -157,7 +157,7 @@
                                           :fastId false
                                           :d-id-re (get-prefix) ;"AABB.*"
                                           :keepalive-ms 60000
-                                          :antennas [[1 29 -69]] ; [2 28 -80]]
+                                          :antennas [[1 30 -70]] ; [2 28 -80]]
                                           :tag-policy {:type :every
                                                        :modul 10
                                                        :wait4 10000
