b1371dd6e04f96b7ee6a0fd434a585dd58444f5d
Felipe de Jesus Gerard Contreras <felipe.gerard.c@gmail.com>
2026-07-09 07:04:08 -0600

    Add query functionality for Rx sensitivity in new namespace

---
+++ src/clj/query_rx_sensivility.clj
@@ -0,0 +1,34 @@
+(ns query-rx-sensitivity
+  (:gen-class)
+  (:import
+   [com.impinj.octane OctaneSdk Reader FeatureSet]))
+
+(defn -main [& args]
+  (when (empty? args)
+    (println "Uso: clj -M -m query-rx-sensitivity <ip-lectora>")
+    (System/exit 1))
+
+  (let [ip (first args)
+        reader (Reader.)]
+
+    (try
+      (.connect reader ip)
+
+      (println "Conectado a" ip)
+
+      (let [features (.queryFeatureSet reader)
+            values (.getRxSensitivities features)]
+
+        (println "Rx Sensitivity soportados:")
+
+        (doseq [v values]
+          (println " " v "dBm")))
+
+      (.disconnect reader)
+
+      (catch Exception e
+        (println (.getMessage e))
+
+        (try
+          (.disconnect reader)
+          (catch Exception _))))))
