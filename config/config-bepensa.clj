baf9e5b722cf994d7b346e8f49f4053288eb0f7b
Felipe de Jesus Gerard Contreras <felipe.gerard.c@gmail.com>
2026-08-28 12:24:07 -0600

    Add error handling for connection loss in event processing

--- config/config-bepensa.clj
+++ config/config-bepensa.clj
@@ -93,7 +93,9 @@
             [:send-tag]
             (smap
              [SE/send-events]
-             (smap [print-it]))))))))))
+             (smap [print-it])))
+           (where [(fn [{:keys [event]}] (or (= event :ON_CONNECTION_LOST) (.exists (io/file "C:/quantumlabs/bepensa-notes/config/stop-server"))))]
+             (smap [(fn [e] (log/error (pr-str ["Connection lost" :-> e])) (System/exit 1))])))))))))
 
 
 ; OJO debemos permitir algun tipo de manejo de las regex por planta mañana lo defino hoy es: (2026-06-03)
